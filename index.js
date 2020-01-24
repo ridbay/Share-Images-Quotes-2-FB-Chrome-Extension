f88f4f9c1a67c1d61b7a785effa9ad6c2356d7cf
Ridwan Balogun <balogunridwan@gmail.com>
2020-01-24 15:38:09 +0100

    Edit extension

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ alert("Loaded")
 
 var dom = document.getElementById("rso");
 console.log("The dom is", dom.innerHTML);
+
+var links = document.getElementsByClassName("main");
+
+for(let i = 0; i < links[i].length; i++){
+    let title = links[i].getElementsByTagName("h3");
+    console.log("Link is", title[0])
+}
