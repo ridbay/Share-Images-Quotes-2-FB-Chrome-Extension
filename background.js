b912544b8129802d36aa60a73d8036d5026f3977
Ridwan Balogun <balogunridwan@gmail.com>
2020-01-24 13:18:54 +0100

    Edit extension

--- background.js
+++ background.js
@@ -3,7 +3,20 @@ alert("hi");
 function MyGenericClick(info, tab){
     console.log("Clicked on page", info, tab);
 }
-chrome.contextmenus.create({
+
+function MyImageClick(info, tab){
+    console.log("Clicked an image", info, tab);
+}
+
+
+chrome.contextMenus.create({
     "title": "Share",
+    "context": ["page"],
     "onclick": MyGenericClick
+})
+
+chrome.contextMenus.create({
+    "title": "Share Image",
+    "context": ["image"],
+    "onclick": MyImageClick
 })
