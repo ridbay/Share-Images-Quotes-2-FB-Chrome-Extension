7ee39fb2dbaf166ebc015854fe2129cc4ddc67dd
Ridwan Balogun <balogunridwan@gmail.com>
2020-01-24 16:51:43 +0100

    To

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+import { DH_CHECK_P_NOT_PRIME } from "constants";
+
 console.log("My extension is running")
 alert("Loaded")
 
 
 var links = document.getElementsByTagName("a");
 
+var formatted_links = [];
 
-for(let i = 0; i < links.length; i++){
+
+for (let i = 0; i < links.length; i++) {
     // let title = links[i].innerHTML;
     let title = links[i].text;
+    let href = links[i].href;
     console.log("Link is", title);
+
+    if (title !== "" && href !== "") {
+        formatted_links.push({ title: title, href: href })
+    }
 }
+
+chrome.runtime.sendMessage({
+    "action": "print_messages",
+    "data": formatted_links
+}, function(res){
+    console.log(res)
+})
