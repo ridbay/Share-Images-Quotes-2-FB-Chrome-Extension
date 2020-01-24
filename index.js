27a245e2d8cd88d981c8638032e61680a009c5b1
Ridwan Balogun <balogunridwan@gmail.com>
2020-01-24 16:42:52 +0100

    To work with any website

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 console.log("My extension is running")
 alert("Loaded")
 
-var dom = document.getElementById("rso");
-// console.log("The dom is", dom.innerHTML);
 
-var links = document.getElementsByClassName("r");
+var links = document.getElementsByTagName("a");
 
 
 for(let i = 0; i < links.length; i++){
-    let title = links[i].getElementsByTagName("h3");
+    let title = links[i].innerHTML;
     console.log("Link is", title[0]);
 }
